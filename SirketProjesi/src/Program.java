6a2f87d85a5677d843809b5928da5f3d04581972
yaren zengin <75395531+yarenzengin@users.noreply.github.com>
2021-04-13 01:56:08 +0300

    Update Program.java

--- SirketProjesi/src/Program.java
+++ SirketProjesi/src/Program.java
@@ -1,14 +1,14 @@
 
 public class Program {
     public static void main(String[] args) {
-        PersonelSinifi yaren = new PersonelSinifi("Yaren  ", "Zengin","yarenzengin65@gmail.com",2001,9655 );
+        PersonelSinifi yaren = new PersonelSinifi("Yaren  ", "Zengin","yaren@gmail.com",1999,9305 );
        yaren.personelYasHesapla();
         System.out.println("*******************");
-       Yazilim eray =  new Yazilim("Java,PHYTON,HTML", "www.erayzengin.github.com", "eray", "zengin","erayzengin65@gmail.com", 2000, 4646);
+       Yazilim eray =  new Yazilim("Java,PHYTON,HTML", "www.erayzengin.github.com", "eray", "zengin","eray5@gmail.com", 2000, 4646);
        eray.yazilimProgramlamaDilleriniGoster();
        eray.personelYasHesapla();
         System.out.println("***************************");
-        Yonetici busra = new Yonetici("bilgi işlem",124,"busra  ", "şenocak","busra65@gmail.com",2001,9045);
+        Yonetici busra = new Yonetici("bilgi işlem",124,"busra  ", "şenocak","busra65@gmail.com",2001,4334);
         busra.yoneticiBilgileriniGoster();
         busra.personelYasHesapla();
         System.out.println("***************************");
